2fb62fd802acf3d132b3977cbcee0fdc2ea185e9
marin.ladovic <marin.ladovic@bozic.io>
2024-07-09 08:50:47 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4 sm:py-4">
+      <h1 className="text-4xl font-bold">Hello, world!</h1>
+      <p className="text-gray-500">
+        This is a React app using TypeScript and TailwindCSS.
+      </p>
+    </div>
+  );
+}
+
+export default App;
