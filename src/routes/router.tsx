0979678467d92a311cabd4ba1ef14c58924db6f0
marin.ladovic <marin.ladovic@bozic.io>
2024-07-09 10:12:59 +0200

    update import aliases

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import LazyLoad from 'shared/lazy-load';
-
-import AppLayout from 'routes/app-layout';
+import AppLayout from '@/routes/app-layout';
+import LazyLoad from '@/shared/lazy-load';
 
 const router = createBrowserRouter([
   {
@@ -11,11 +10,11 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <LazyLoad loader={() => import('features/home')} />
+        element: <LazyLoad loader={() => import('@/features/home')} />
       },
       {
         path: 'second-page',
-        element: <LazyLoad loader={() => import('features/second-page')} title="Second Page" />
+        element: <LazyLoad loader={() => import('@/features/second-page')} title="Second Page" />
       }
     ]
   }
